d544e27db524f8ca6254910cc0441ce9fccba826
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-10-27 13:47:21 -0400

    Submitting Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+
+const employee = {
+        name: "Andrew",
+        streetAddress: "299 Adelphi"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(
+    employee, "name", "Sam",
+    employee, "streetAddress", "11 Broadway"
+)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    
+    const newEmployee = Object.assign({}, employee)
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+deleteFromEmployeeByKey(newEmployee, "name");
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+destructivelyDeleteFromEmployeeByKey(employee, "name");
